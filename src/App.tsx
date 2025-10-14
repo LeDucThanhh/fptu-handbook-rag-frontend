3ff881d725ae1afdaa4a3f3778b60893e866eab5
soufnotfunny <leminhtam31104@gmail.com>
2025-10-15 00:36:55 +0700

    Refactor App component to implement a new routing structure with separate layouts for student and mentor pages. Remove Dashboard component and integrate Mentor routes under a new MentorLayout. Add default redirect to home route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import MentorLayout from "./components/MentorLayout";
 import Home from "./pages/Home";
 import QA from "./pages/QA";
 import Clubs from "./pages/Clubs";
@@ -9,7 +10,6 @@ import Handbook from "./pages/Handbook";
 import Introduction from "./pages/Handbook/Introduction";
 import Admission from "./pages/Handbook/Admission";
 import Tuition from "./pages/Handbook/Tuition";
-import Dashboard from "./pages/Mentor/Dashboard";
 import Analytics from "./pages/Mentor/Analytics";
 import ResourceRecommendation from "./pages/Mentor/ResourceRecommendation";
 import UnresolvedQueue from "./pages/Mentor/UnresolvedQueue";
@@ -17,26 +17,92 @@ import UnresolvedQueue from "./pages/Mentor/UnresolvedQueue";
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-background flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/qa" element={<QA />} />
-            <Route path="/clubs" element={<Clubs />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/handbook" element={<Handbook />} />
-            <Route path="/handbook/introduction" element={<Introduction />} />
-            <Route path="/handbook/admission" element={<Admission />} />
-            <Route path="/handbook/tuition" element={<Tuition />} />
-            <Route path="/mentor" element={<Dashboard />} />
-            <Route path="/mentor/analytics" element={<Analytics />} />
-            <Route path="/mentor/recommendations" element={<ResourceRecommendation />} />
-            <Route path="/mentor/queue" element={<UnresolvedQueue />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        {/* Student Routes with Navbar and Footer */}
+        <Route path="/home" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Home />
+            </main>
+            <Footer />
+          </div>
+        } />
+        <Route path="/qa" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <QA />
+            </main>
+            <Footer />
+          </div>
+        } />
+        <Route path="/clubs" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Clubs />
+            </main>
+            <Footer />
+          </div>
+        } />
+        <Route path="/faq" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <FAQ />
+            </main>
+            <Footer />
+          </div>
+        } />
+        <Route path="/handbook" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Handbook />
+            </main>
+            <Footer />
+          </div>
+        } />
+        <Route path="/handbook/introduction" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Introduction />
+            </main>
+            <Footer />
+          </div>
+        } />
+        <Route path="/handbook/admission" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Admission />
+            </main>
+            <Footer />
+          </div>
+        } />
+        <Route path="/handbook/tuition" element={
+          <div className="min-h-screen bg-background flex flex-col">
+            <Navbar />
+            <main className="flex-grow">
+              <Tuition />
+            </main>
+            <Footer />
+          </div>
+        } />
+
+        {/* Mentor Routes with Sidebar Layout */}
+        <Route path="/mentor" element={<MentorLayout />}>
+          <Route index element={<Navigate to="/mentor/analytics" replace />} />
+          <Route path="analytics" element={<Analytics />} />
+          <Route path="recommendations" element={<ResourceRecommendation />} />
+          <Route path="queue" element={<UnresolvedQueue />} />
+        </Route>
+
+        {/* Default redirect */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
+      </Routes>
     </Router>
   );
 }
